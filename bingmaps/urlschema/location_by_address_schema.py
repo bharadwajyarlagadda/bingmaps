b73184a949557d2391913a722513cac6f4819459
Bharadwaj <yarlagaddabharadwaj@gmail.com>
2016-03-09 00:35:16 -0500

    Inherited LocationUrl to LocationByAddressUrl class

--- bingmaps/urlschema/location_by_address_schema.py
+++ bingmaps/urlschema/location_by_address_schema.py
@@ -1,4 +1,6 @@
 from marshmallow import Schema, fields, post_dump
+from .location_schema import Location
+from .location_api_url import LocationUrl
 
 
 class LocationByAddressQueryString(Schema):
@@ -79,16 +81,6 @@ class LocationByAddressQueryString(Schema):
         return "&".join(query_params)
 
 
-class Location(Schema):
-    version = fields.Str(
-        default='v1'
-    )
-    restApi = fields.Str(
-        default='Locations'
-    )
-    resourcePath = fields.Str()
-
-
 class LocationByAddressSchema(Location, Schema):
     queryParameters = fields.Nested(
         LocationByAddressQueryString
@@ -99,55 +91,10 @@ class LocationByAddressSchema(Location, Schema):
         ordered = True
 
 
-class LocationByAddressUrl(object):
+class LocationByAddressUrl(LocationUrl):
     def __init__(self, data, protocol):
-        self._data = data
-        self._http_protocol = protocol
-        self._schema_dict = self._schema_values()
-
-    def _schema_values(self):
         schema = LocationByAddressSchema()
-        is_valid_schema = schema.validate(self._data)
-        if bool(is_valid_schema):
-            raise KeyError(is_valid_schema)
-        else:
-            return schema.dump(self._data).data
-
-    @property
-    def protocol(self):
-        if self._http_protocol == 'http':
-            return 'http:/'
-        else:
-            return 'https:/'
-
-    @property
-    def main_url(self):
-        return 'dev.virtualearth.net'
-
-    @property
-    def rest(self):
-        return 'REST'
-
-    @property
-    def version(self):
-        if 'version' in self._schema_dict:
-            return self._schema_dict['version']
-        else:
-            return ''
-
-    @property
-    def restApi(self):
-        if 'restApi' in self._schema_dict:
-            return self._schema_dict['restApi']
-        else:
-            return ''
-
-    @property
-    def resourcePath(self):
-        if 'resourcePath' in self._schema_dict:
-            return self._schema_dict['resourcePath']
-        else:
-            return ''
+        super().__init__(data, protocol, schema)
 
     @property
     def query(self):
